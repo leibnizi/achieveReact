9f498352e99f7c66ca5af48f92c1dfe00e57bea0
wedge <wedgechiang18@gmail.com>
2018-10-02 14:36:24 +0800

    ✨ add class

--- index.js
+++ index.js
@@ -15,8 +15,37 @@
 }
  */
 
-function createElement(parentEle, props, ...childEles) {
+/**
+  * 
+  * @param {*} parentEle 
+  * @param {*} props 
+  * @param  {...any} childEles 
+  * step3
+  function createElement(parentEle, props, ...childEles) {
   if (typeof parentEle === "function") {
+    return parentEle();
+  } else {
+    let parentElement = document.createElement(parentEle);
+    childEles.forEach(child => {
+      if (typeof child === "string") {
+        parentElement.innerHTML += child;
+      } else if (typeof child === "object") {
+        parentElement.appendChild(child);
+      }
+    });
+    return parentElement;
+  }
+}
+  */
+
+function createElement(parentEle, props, ...childEles) {
+  if (
+    typeof parentEle === "function" &&
+    /^\s*class\s+/.test(parentEle.toString())
+  ) {
+    let component = new parentEle();
+    return component.render();
+  } else if (typeof parentEle === "function") {
     return parentEle();
   } else {
     let parentElement = document.createElement(parentEle);
@@ -52,7 +81,9 @@ ReactDOM.render(helloWorld, document.getElementById("root"));
 
  */
 
-const HelloVersion3 = function() {
+/**
+ * step3
+ const HelloVersion3 = function() {
   return React.createElement("div", null, `版本3.0`);
 };
 const helloWorld1 = React.createElement(HelloVersion3, null, null);
@@ -68,5 +99,13 @@ const parent = React.createElement(
   divEle,
   `我是文本内容哦`
 );
+ */
 
-ReactDOM.render(parent, document.getElementById("root"));
+class Hello {
+  render() {
+    return React.createElement("div", null, `版本四，类组件的实现`);
+  }
+}
+const helloWorld = React.createElement(Hello, null, null);
+
+ReactDOM.render(helloWorld, document.getElementById("root"));
